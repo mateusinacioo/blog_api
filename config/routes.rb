53a9045e7f1c778986e76188d9aa817abc2a78b5
mateusinacioo <mateus.inacio321@gmail.com>
2022-10-22 14:40:54 -0300

    Criado usuário, sign in, sign out

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   namespace :api, defaults: {format: :json} do
     mount_devise_token_auth_for 'User', at: 'auth'
+    
     scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: false) do
       resources :articles
